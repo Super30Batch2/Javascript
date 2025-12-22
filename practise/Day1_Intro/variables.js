82594e70bba8f68b2183bb4b370a898117dcad82
TheAsh009 <ashitoshpatil27@gmail.com>
2025-12-22 13:02:42 +0530

    first commit js basics

---
+++ practise/Day1_Intro/variables.js
@@ -0,0 +1,18 @@
+//the ways of declaring variables in js
+// let a = 10;
+// const b = 10;
+// var c = 20;
+
+//typeof operator
+//primary datatypes
+//null undefined
+
+console.log(10 == "10");
+console.log(10 === 10);
+
+console.log(10 + "10");
+
+console.log(10 - "10");
+
+///refrence error variable is not defined
+console.log(a + "a");
