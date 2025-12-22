9cfc6c3abbdcf560714885144912429361c4df26
Rkulkrni <rahulspk7@gmail.com>
2025-12-22 11:18:21 +0530

    Day_1_Introduction, datatype, variables

---
+++ study/Day 1 JS_intro/syntax.js
@@ -0,0 +1,26 @@
+
+console.log("Hello Javascript");
+
+// variables : 
+// 1) var, let, const.
+var data = 'codefirst';
+var data = "codefirst";
+let test = 'india';
+const test2 = 'usa';
+
+const testCase21 = '';
+
+// let a;
+// console.log(a);
+// console.log(typeof(a));
+
+
+const obj = {
+    name : 'rk',
+    isValid : true
+}
+
+const arr = [2,4,4,];
+
+
+
