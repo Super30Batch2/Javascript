8b74c655e990e420e66d6b25abb7a988c663590c
TheAsh009 <ashitoshpatil27@gmail.com>
2025-12-22 20:22:49 +0530

    day1 final practise

--- practise/Day1_Intro/variables.js
+++ practise/Day1_Intro/variables.js
@@ -11,72 +11,124 @@
 
 //null undefined
 
-console.log(10 == "10", `${10 == "10"}`);
-//true
+// console.log(10 == "10", `${10 == "10"}`);
+// //true
 
-console.log(10 === 10, `${10 === 10}`);
-//true
+// console.log(10 === 10, `${10 === 10}`);
+// //true
 
-console.log(10 + "10", 10 + "10");
-// 1010
+// console.log(10 + "10", 10 + "10");
+// // 1010
 
-console.log(10 - "10", `${10 - "10"}`);
-// 0
+// console.log(10 - "10", `${10 - "10"}`);
+// // 0
 
-///refrence error variable is not defined
-// console.log(a + "a");
+// ///refrence error variable is not defined
+// // console.log(a + "a");
 
-//examples done
-console.log("10" - 2 + "5", `${"10" - 2 + "5"}`);
-//85
+// //examples done
+// console.log("10" - 2 + "5", `${"10" - 2 + "5"}`);
+// //85
 
-console.log(2 + "2" - 2, `${2 + "2" - 2}`);
-//20
+// console.log(2 + "2" - 2, `${2 + "2" - 2}`);
+// //20
 
-console.log("a" + 5, `${"a" + 5}`);
-//a5
+// console.log("a" + 5, `${"a" + 5}`);
+// //a5
 
-console.log(5 + "a" + 5, `${5 + "a" + 5}`);
-//5a5
+// console.log(5 + "a" + 5, `${5 + "a" + 5}`);
+// //5a5
 
-console.log("a" + true, `${"a" + true}`);
-//ature
+// console.log("a" + true, `${"a" + true}`);
+// //ature
 
-console.log("a" + null, `${"a" + null}`);
-//anull
+// console.log("a" + null, `${"a" + null}`);
+// //anull
 
-console.log("a" + undefined, `${"a" + undefined}`);
-//aundefined
+// console.log("a" + undefined, `${"a" + undefined}`);
+// //aundefined
 
-console.log("a" - 5, `${"a" - 5}`);
-//nan
+// console.log("a" - 5, `${"a" - 5}`);
+// //nan
 
-console.log("a" * 4, `${"a" * 4}`);
-//nan
+// console.log("a" * 4, `${"a" * 4}`);
+// //nan
 
-console.log("a" / 2, `${"a" / 2}`);
-//nan
+// console.log("a" / 2, `${"a" / 2}`);
+// //nan
 
-console.log("a" + "b", `${"a" + "b"}`);
-//ab
+// console.log("a" + "b", `${"a" + "b"}`);
+// //ab
 
-console.log("a" - "b", `${"a" - "b"}`);
-//nan
+// console.log("a" - "b", `${"a" - "b"}`);
+// //nan
 
-console.log("4" - "a", `${"4" - "a"}`);
-//nan
+// console.log("4" - "a", `${"4" - "a"}`);
+// //nan
 
-console.log(10 + "a" - 2, `${10 + "a" - 2}`);
-//nan
+// console.log(10 + "a" - 2, `${10 + "a" - 2}`);
+// //nan
 
-console.log("a" + 10 - 5, `${"a" + 10 - 5}`);
-//nan
+// console.log("a" + 10 - 5, `${"a" + 10 - 5}`);
+// //nan
 
-console.log("3a" - 1, "3a - 1");
-//nan
+// console.log("3a" - 1, "3a - 1");
+// //nan
 
-console.log("a" > "b", "a > b");
-//false
+// console.log("a" > "b", "a > b");
+// //false
 
-console.log("b" > "a", "b > a");
-//true
+// console.log("b" > "a", "b > a");
+// //true
+
+// console.log(1, 23, 4, 5, 6);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.join(""));
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+//you have 2 hrs to do this after that check html css bootstrap theory
+
+//check for today
+//let const var
+//what is Js Advantages limitations
+//and check the typeof all the primitive as well non primitive data types
+
+// const function1 = function () {
+//   const a;
+//   if (a === 11) {
+//     // let b = 12;W
+//     a = 122;
+//     console.log("The value of a is under if", a);
+//   }
+//   //   console.log("The value of b otside the block", b);
+// };
+
+// function1();
+
+// var a =10
+// var a=12;
+
+console;
+
+//true it perfoms typeconersion
+console.log(11 == "11");
+
+let a;
+console.log(a);
+
+let b = null;
+
+// it does not perform type coresion
+//fasle
+console.log(11 === "111");
+
+// type conversion
+console.log(Number("11"));
+
+let c = 12;
+
+let d = c;
