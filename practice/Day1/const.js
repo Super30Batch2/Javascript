a2db5c04ecf220e298ff716c066b6452c92465b4
yogeshweb89 <yogeshs98@gmail.com>
2025-12-23 10:16:22 +0530

    javascript_day1

---
+++ practice/Day1/const.js
@@ -0,0 +1,9 @@
+const a=52;
+// a=30; 
+console.log(a);
+{
+    a=40;
+// a=40;
+//TypeError: Assignment to constant variable.
+}
+console.log(a);
---
+++ practice/Day1/datatypes.js
@@ -0,0 +1,52 @@
+//1.number
+let num=10;
+let a=25.6;
+let b= 0.56;
+console.log(num);
+console.log(typeof(num));
+console.log(typeof(a));
+console.log(typeof(b));
+
+//2.strings
+
+let dt="Mumbai";
+let wt='Mumbai';
+let et=`Mumbai`;
+
+console.log(typeof(dt),dt);
+console.log(typeof(wt),wt);
+console.log(typeof(et),et);
+
+//3.boolean
+
+let m=true;
+let d=false;
+console.log(typeof(m),m);
+console.log(typeof(d),d);
+
+//boolean//notes
+
+var test1 = true;
+
+console.log(test1);
+console.log(typeof(test1));//boolean
+console.log(!test1);
+console.log(test1.valueOf());
+const value = test1.toString();
+console.log(typeof(value));
+console.log(test1.toString());
+
+//4.undefined
+
+let user;//value not assign to variable then it will by default show undefined
+console.log(user);
+
+//5.null
+let user2=null;
+console.log(user2);
+console.log(typeof(user2));//type of null object
+
+
+
+
+
