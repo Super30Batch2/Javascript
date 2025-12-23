1ea7936295974ef015eafba04481f7762cfb2a9d
samruddhi hande <samruddhih454@gmail.com>
2025-12-23 08:01:44 +0530

    day1

---
+++ practice/day1/script.js
@@ -0,0 +1,65 @@
+// const { jsx } = require("react/jsx-runtime");
+
+// let str1 = "hGHRTYUIOPJBVFTYUIlo";
+// //to lower case
+// console.log(str1.toLowerCase());
+// //Extract the first 5 characters from a string.
+// console.log(str1.substring(0,6));
+// //Check whether a string contains the word "JavaScript".
+// let str2 = 'sahhhidddghqosiuh ajdhai javaScript';
+// let str3 = str2.search("javaScript");
+// console.log(str3);
+// //Replace all spaces in a string with -.
+// let str4 = str2.replace(' ' , '-');
+// console.log(str4);
+
+// //Reverse a string using JavaScript.
+// // let str5  = str2.split('');
+// // let str7 = str5.replace(str5[1],str5[8])
+
+
+// //Convert a string "123" into a number
+
+
+// var test5 = '$'+ 5;
+// console.log(test5);
+// console.log(typeof(test5));
+
+
+
+// let n = '33333333';
+// let k ='3333';
+
+// let num = Number(n);
+// console.log(typeof(num))
+
+// number + string = numString
+let w = 134;
+let o = '12';
+let sum = w + o;
+console.log(typeof(sum));
+//string
+
+// string + num = string
+// num + string + num = string
+// bol + num = num
+let a = true;
+let b = 2;
+console.log(a+b);//3
+console.log(typeof(a+b));//number
+
+console.log(a*b);
+console.log(w*o);
+console.log(typeof(w*o));
+console.log(w*true);
+console.log(o*true);
+//typeof()
+console.log(typeof(o*true));
+
+let stri = 'a2b3c4d4'
+stri*true
+console.log(typeof(stri*true));
+console.log(stri*true);
+
+let f = stri.charAt(0);
+console.log(typeof(f));
