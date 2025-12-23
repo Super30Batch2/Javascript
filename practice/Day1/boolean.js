06f087a33c1ad8044762cbf9818502782948e2ab
shivraj7100 <shivrajjadhav710@gmail.com>
2025-12-23 10:11:25 +0530

    javascript day1 practice(Data Type, Variable)

---
+++ practice/Day1/boolean.js
@@ -0,0 +1,9 @@
+var test1 = true;
+
+// console.log(test1);
+console.log(typeof(test1))//boolean
+// console.log(!test1);
+// console.log(test1.valueOf());
+const value = test1.toString();
+console.log(typeof(value));
+// console.log(test1.toString());
---
+++ practice/Day1/syntax.js
@@ -0,0 +1,31 @@
+// 1)
+console.log("Hello Javascript");
+
+// variables : 
+// 2) var, let, const.
+var data = 'codefirst';
+var data = "codefirst";
+
+// 3)let
+let test = 'india';
+
+// 4)const
+const test2 = 'usa';
+const testCase21 = '';
+
+// 5) type check
+let a;
+console.log(a);
+console.log(typeof(a));
+
+// 6) object
+const obj = {
+    name : 'rk',
+    isValid : true
+}
+
+// 7)
+const arr = [2,4,4,5];
+
+
+
