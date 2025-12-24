c48363b29fee0b9c1858365be3f2bdc8910b7885
priyankatapkeer <priyanka.tapkeer2426@gmail.com>
2025-12-24 09:54:06 +0530

    variable&opertors practie

---
+++ practie/arithmeticOpertors.js
@@ -0,0 +1,52 @@
+console.log("Arithmetic Opertors :");
+
+let a=20;
+let n =30;
+// console.log('Arithmetic Opertors :',a+n);
+// console.log('Subtraction :',a-n);
+// console.log('multiplication :',a*n);
+// console.log('Division :', a/n);
+// console.log('Modulus :', a%n);
+
+let first =20;
+let second = 5;
+
+let inc = ++first;
+// console.log("Increament = ",inc);
+
+var dec = --second;
+// console.log("Decreament = ",dec);
+
+// console.log(!true);
+
+let d = "case";
+// console.log(typeof(d));
+
+let c=(first>second)?true:false;
+
+let data = 30;
+let data1 = 40;
+let data4 = '100';
+
+// let data2= data==data1;
+// console.log(data2);
+
+// let data3= data===data1;
+// console.log(data3);
+
+// let test3 = data===data4;
+// console.log(test3);
+
+// let test = data & data1;
+// console.log(test);
+
+// let test1 = data |data1;
+// console.log(test1);
+// let test2 = data!=data1;
+// console.log(test2);
+
+let name='shiv';
+let name1='ram';
+
+let result = (name==shiv)?'both are same':'both are not same';
+
