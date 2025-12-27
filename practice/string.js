f8c4e3dba1589464359cc54c90a98c901aadd2ab
priyankatapkeer <priyanka.tapkeer2426@gmail.com>
2025-12-27 19:59:13 +0530

    string

---
+++ practice/string.js
@@ -0,0 +1,54 @@
+// console.log("hello");
+
+var string = "This Is String";
+
+
+//charAt.
+const char = string.charAt(3);
+console.log(char);
+
+// concatination 
+const con = string + ' In javascript';
+console.log(con);
+
+// indexof 
+const index = string.indexOf("is");
+console.log(index);
+
+
+// replace 
+var rep= string.replace('String','javascript');
+console.log(rep);
+
+// uppercase 
+const upper = string.toUpperCase();
+console.log(upper);
+
+// lowercase
+const lower = string.toLowerCase();
+console.log(lower);
+
+
+// substring
+const sub = string.substring(1,6);
+console.log(sub);
+
+// search
+const search=string.search("is");
+console.log(search);
+
+// slice(starting index,end index-1)
+const slice = string.slice(2,6);
+console.log(slice);
+
+// split
+const split =string.split();
+console.log(split);
+
+// lengthSS
+console.log(string.length);
+
+
+
+
+
