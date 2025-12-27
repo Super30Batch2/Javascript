76edcf07793af6e516b511ce2cf1e81da043367d
priyankatapkeer <priyanka.tapkeer2426@gmail.com>
2025-12-27 19:53:26 +0530

    boolean

---
+++ practice/boolean.js
@@ -0,0 +1,15 @@
+var data = true;
+
+console.log(data);
+console.log(typeof(data));
+// console.log(data);
+// console.log(data.valueOf());
+
+/* * boolean convart to string  */
+const  res = data.toString();
+console.log(typeof(res));
+
+console.log(data.toString());
+
+
+
