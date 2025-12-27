abe01fa3e6c5a82b898951f053118c5f390b1051
priyankatapkeer <priyanka.tapkeer2426@gmail.com>
2025-12-27 19:54:28 +0530

    conditionOperator

---
+++ practice/conditionOperator.js
@@ -0,0 +1,45 @@
+console.log("Hello");
+
+// let a = 10;
+
+// if(a!=2){
+//     console.log('a is even');
+// }
+
+// var x = 20;
+// var z = 25;
+
+// if(x>z){
+//     console.log('greater then');
+// }else{
+//     console.log('less then');
+// }
+
+// var name1;
+
+// switch(name1){
+//      case 1:
+//         name1='shiv';
+//         break;
+    
+//     case 2:
+//         name1='raghav';
+//         break;
+
+//     case 3 :
+//         name1='krishana';
+//         break;
+
+//      default:
+//         name1= 'this is not matching';
+//         break;
+// }
+// console.log(name1);
+
+
+// var count=1;
+
+// while(count<10){
+//     count++;
+//     console.log(count);
+// }
