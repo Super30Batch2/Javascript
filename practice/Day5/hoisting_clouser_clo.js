7c0044037808eae294a12983bcddf141d5c88181
shivraj7100 <shivrajjadhav710@gmail.com>
2025-12-30 09:24:21 +0530

    hoisting_clouser

---
+++ practice/Day5/hoisting_clouser_clo.js
@@ -0,0 +1,18 @@
+// what is hoisting?
+//
+console.log("Hoisting:",value);
+var value = 10;
+
+// What is clouser?
+// We can access outer function varibale into inner function but vise varse we can't.
+
+function outer() {
+    const outerVariable = 100;
+    console.log(innerVariable);
+    function innner() {
+        var innerVariable = 500;
+        console.log(outerVariable);
+    }
+    innner();
+}
+outer();
