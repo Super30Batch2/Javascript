de92de7f6a3d6986691a3e0295aa62035585b7e1
shivraj7100 <shivrajjadhav710@gmail.com>
2025-12-30 11:28:19 +0530

    practise hoisting closure

---
+++ practice/Day6/project/Form.js
@@ -0,0 +1,10 @@
+function test(){
+  var uid = document.getElementById("username").value;
+  var em =  document.getElementById("email").value;
+  var pn =  document.getElementById("phone").value;
+
+  alert(uid + em + pn);
+
+}
+
+document.getElementById("myform").value;
